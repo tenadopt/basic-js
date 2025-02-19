220780c853e63a503905711559741e42f9d41648
Tenadopt <yury.ignatiev@gmail.com>
2025-02-19 12:17:19 +0100

    feat: add solution to the task Tower of Hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -4,7 +4,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
  * 
- * @param {Number} disks number of disks
+ * @param {Number} disksNumber number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor(turns / (turnsSpeed / 3600));
+
+  console.log('turns', turns);
+  console.log('seconds', seconds);
+
+  return { turns, seconds };
 }
 
 module.exports = {
